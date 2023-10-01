48b8ef2d16ce3dc573229bee49eeddfc6c4de8e9
shirvan <shirvan-1@mail.ru>
2023-10-01 15:29:02 +0300

    backup

--- src/resources/backup/old/old1/uninstall.sh
+++ src/resources/backup/old/old1/uninstall.sh
@@ -7,11 +7,11 @@ echo "stopping zookeeper"
 sudo su - kafka /tmp/old1/scripts/zkStop.sh
 
 echo "remove directories"
-rm -rf /opt/kafka/current
-rm -rf /opt/kafka/kafka_2.13-3.1.0
-rm -rf /opt/zookeeper/current
-rm -rf /opt/zookeeper/apache-zookeeper-3.9.0-bin
+rm -rf /opt/kafka
+rm -rf /opt/zookeeper
 rm -rf /opt/java
 rm -rf /opt/jdk-11.0.2/
+rm -rf /opt/scripts
+rm -rf /opt/migration
 
 echo "uninstall completed!"
--- src/resources/backup/old/old3/reinstall.sh
+++ src/resources/backup/old/old3/reinstall.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-echo "stopping kafka"
-sudo su - kafka /tmp/old3/scripts/kafka-stop.sh
-
-echo "stopping zookeeper"
-sudo su - kafka /tmp/old3/scripts/zkStop.sh
-
-echo "remove directories"
-rm -rf /opt/kafka/current
-rm -rf /opt/kafka/kafka_2.13-3.1.0
-rm -rf /opt/zookeeper/current
-rm -rf /opt/zookeeper/apache-zookeeper-3.9.0-bin
-rm -rf /opt/java
-rm -rf /opt/jdk-11.0.2/
+echo "uninstalling..."
+./uninstall.sh
 
 echo "installing..."
 ./setup.sh

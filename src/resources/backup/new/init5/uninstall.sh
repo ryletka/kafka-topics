aacc8d27ade23dba87c856464885345dcaa850c1
shirvan <shirvan-1@mail.ru>
2023-10-01 15:31:42 +0300

    backup

--- src/resources/backup/new/init5/uninstall.sh
+++ src/resources/backup/new/init5/uninstall.sh
@@ -7,11 +7,10 @@ echo "stopping zookeeper"
 sudo su - kafka /tmp/init5/scripts/zkStop.sh
 
 echo "remove directories"
-rm -rf /opt/kafka/current
-rm -rf /opt/kafka/kafka_2.13-3.2.0
-rm -rf /opt/zookeeper/current
-rm -rf /opt/zookeeper/apache-zookeeper-3.9.0-bin
+rm -rf /opt/kafka
+rm -rf /opt/zookeeper
 rm -rf /opt/java
 rm -rf /opt/jdk-11.0.2/
+rm -rf /opt/scripts
 
 echo "uninstall completed!"
